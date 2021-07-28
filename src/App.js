0965baef655e1f15c06573aad72a1e38a2f28f24
josue <josue@gmail.com>
2021-07-28 16:45:17 -0500

    Subiendo

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Angular from './img/3662358_4d6e.jpg';
+function App() {
+  return (
+    <>
+      
+        <img src={Angular} alt="IMAGENES" />
+
+
+
+
+
+
+
+
+
+
+        <br />
+
+
+
+
+        Hola
+      
+    </>
+  );
+}
+
+export default App;
